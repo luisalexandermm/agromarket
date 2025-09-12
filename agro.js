43f834a1ba11a661559d7232b2e9eee29df81e78
luisalexandermm <alrxandermaturana76@gmail.com>
2025-09-11 19:45:04 -0500

    incor3

--- agro.js
+++ agro.js
@@ -682,7 +682,7 @@ class MercadoQuibdoApp {
 
         // Abrir WhatsApp
         const encodedMessage = encodeURIComponent(message);
-        const whatsappUrl = `https://wa.me/573145312045?text=${encodedMessage}`;
+        const whatsappUrl = `https://wa.me/57 322 6654844?text=${encodedMessage}`;
         window.open(whatsappUrl, '_blank');
 
         // Limpiar carrito después del pedido
